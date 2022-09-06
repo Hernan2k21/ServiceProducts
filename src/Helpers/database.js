de9b359c292a41851a6290609e1dad699102920e
Hernan2k21 <hernan@genosha.com.ar>
2022-09-06 11:03:13 -0300

    update db to atlas

--- src/Helpers/database.js
+++ src/Helpers/database.js
@@ -5,7 +5,7 @@ const config = require('../../config/app.config')['database'];
 async function initDatabase () {
 
         try{
-          const uri = `mongodb://${config.username}:${config.password}@${config.host}:${config.port}/?retryWrites=true&w=majority`
+          const uri = `mongodb+srv://${config.username}:${config.password}@${config.host}/?retryWrites=true&w=majority`
           connect(uri, { useNewUrlParser: true })
           console.log("Connected to db")
         }
